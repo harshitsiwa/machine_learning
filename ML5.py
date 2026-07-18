5635c7473c5da64a51905dc3b9d4ddc4b8af5fed
harshitsiwa <harshitsiway0641@gmail.com>
2026-07-18 22:36:41 +0530

    Feature Scaling

--- ML5.py
+++ ML5.py
@@ -1,36 +1,36 @@
+# Import necessary libraries
 import pandas as pd
 import numpy as np
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import LabelEncoder
-from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
 
-dataset= pd.read_csv("Data.csv")
+
+# Load the Wine Quality Red dataset
+dataset= pd.read_csv("winequality.csv")
 x= dataset.iloc[:, :-1].values
 y= dataset.iloc[:, -1].values
-
-imputer= SimpleImputer(missing_values=np.nan, strategy= "mean")
-x[:, 1:3]= imputer.fit_transform(x[:, 1:3])
 print(x)
+print(y,"\n")
 
-#categorical_features= ['Country','Purchased']
-ct= ColumnTransformer(transformers=[("encoder",OneHotEncoder(),[0])], remainder="passthrough")
-x=np.array(ct.fit_transform(x))
+#Separate features and target
+features= ["fixed acidity","volatile acidity","citric acid","residual sugar","chlorides","free sulfur dioxide","total sulfur dioxide","density","pH","sulphates","alcohol"]
+target= ["quality"]
 
-le= LabelEncoder()
-y=le.fit_transform(y)
-print(x,"\n")
-print(y)
+#handling missing values
+imputer= SimpleImputer(missing_values=np.nan, strategy= "mean")
+x[:, 0:]= imputer.fit_transform(x[:, 0:])
 
-x_train,x_test,y_train,y_test= train_test_split(x,y, test_size=0.2)
-print(x_train,"\n")
-print(x_test)
 
-#Feature Scaling
+# Split the dataset into an 80-20 training-test set
+x_train, x_test, y_train, y_test= train_test_split(x,y, test_size=0.2)
+print(x_train,"\n", x_test,"\n", y_train,"\n", y_test,"\n till here")
+
+# Create an instance of the StandardScaler class
 sc= StandardScaler()
-x_train[:, 3:]= sc.fit_transform(x_train[:, 3:])
-x_test[:, 3:]= sc.fit_transform(x_test[:, 3:])
-print(x_train,"\n")
-print(x_test)
+
+x_train[:, :]= sc.fit_transform(x_train[:, :])
+x_test[:, :]= sc.transform(x_test[:, :])
+
+print(x_train,"\n",x_test)
+print(y_train,"\n",y_test)
